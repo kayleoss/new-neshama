2cd3cea7cd7a6b3e7ca120663add389f21997387
Duo Liu <duoliu@Duos-MacBook-Pro-2.local>
2018-11-18 00:17:32 -0500

    add posting functionality & debug

---
+++ includes/post-resolve.php
@@ -0,0 +1,17 @@
+<?php 
+	ini_set('display_errors', 1);
+	ini_set('display_startup_errors', 1);
+	error_reporting(E_ALL);
+	include "../layouts/head.php";
+	include_once "../includes/db.php";
+	if (isset($_POST['input']) && strlen($_POST['input']) >= 10 && strlen($_POST['input']) < 8000 )  {
+	 	$text = mysqli_real_escape_string($conn, $_POST['input']);
+	 	$sql = "INSERT INTO posts (text) VALUES ('$text')";
+      	mysqli_query($conn, $sql);
+      	header('Location: /pages/hub?success');
+	 } else if (isset($_POST['input']) && strlen($_POST['input']) < 10) {
+	 	header('Location: /pages/hub?failure=short');
+	 } else if (isset($_POST['input']) && strlen($_POST['input']) >= 8000) {
+	 	header('Location: /pages/hub?failure=long');
+	 }
+?>
--- pages/hub.php
+++ pages/hub.php
@@ -1,11 +1,9 @@
 <?php 
-	ini_set('display_errors', 1);
-	ini_set('display_startup_errors', 1);
-	error_reporting(E_ALL);
+
 	$page_title = "Neshama Therapy: Hub - A place for health and wellness sharing";
 	$book_now_button = false;
 	$banner_heading = "Neshama Hub";
-	$banner_text = "This is a forum for all discussion related to health and wellness! If you have related comments/questions or would like to share health tips, this is a safe place to do so.";
+	$banner_text = "A community for all discussion related to health & wellness, and if you have related comments/questions or would like to share health tips";
 	$banner_img_src = "https://ar.unesco.org/sites/default/files/banner_ifap_938x488.jpg";
 	include "../layouts/head.php";
 	include "../layouts/nav.php";	
@@ -23,37 +21,36 @@
 	      $posts[] = $post;
 	    }
 	  }
+
+	 if (isset($_GET['success'])) {
+	 	echo "<p class='text-center mt-3 mb-0 text-success'>Post success</p>";
+	 } else if (isset($_GET['failure']) && $_GET['failure'] == 'short' ) {
+	 	echo "<p class='text-center mt-3 mb-0 text-danger'>Failed to submit. Post was too short.</p>";
+	 } else if (isset($_GET['failure']) && $_GET['failure'] == 'long' ) {
+	 	echo "<p class='text-center mt-3 mb-0 text-danger'>Failed to submit. Post must be under 8000 characters.</p>";
+	 }
+	 
 ?>
 <section class="container p-5">
 	<section class="row bg-main-transparent container product-border p-3">
 		<h3 class="text-center text-light col-sm-12 mt-3">Sharing & Discussion Board</h3>
 		<div class="col-sm-8">
-			<div class="row">
+			<div class="row mt-3 pb-3">
 				<?php foreach ($posts as $p): ?>
-					<div class="col-sm-4">
-						<div class="speech-bubble p-3">
-							<?php echo $posts[0]->text; ?>
-						</div>
+					<div class="col-sm-4 speech-bubble p-3">
+						<?php echo $p->text; ?>
 					</div>
 				<?php endforeach; ?>	
 			</div>
 		</div>
 		<div class="col-sm-4">
-			<textarea class="block form-control p-2 mt-3 mb-3 w-75 mr-auto ml-auto" data-text-input placeholder="Share what's on your mind!"></textarea>
-			<button class="navbar-toggler btn-custom-light block m-auto text-light" data-submit>Post</button>
+			<form action="/includes/post-resolve.php" method="POST">
+				<textarea class="block form-control p-2 mt-3 mb-3 w-75 mr-auto ml-auto" data-text-input placeholder="Share what's on your mind..." name="input" required></textarea>
+				<button type="submit" class="navbar-toggler btn-custom-light block m-auto text-light" data-submit>Post</button>
+			</form>
 		</div>
 	</section>
 </section>
-<script>
-	$('button[data-submit]').on('click', function() {
-		var input = $('textarea[data-text-input]').val();
-		if (input.length > 1) {
-			//handle post here
-		} else {
-			alert("You tried to submit an empty post.")
-		}
-	});
-</script>
 <section class="mt-5 p-5 therapist-intro">
 	<div class="text-center">
 		<h4 class="text-light weight-bold">Want to join the discussion with certified RMT and TCM therapists?<br>Join the Neshama Therapist Hub!</h4>
